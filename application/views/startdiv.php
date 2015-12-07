3403defe4e304812445f7f5fa5aec29f96fdc9a2
Rohan Khanna <rohan.extra@gmail.com>
2015-12-08 03:44:25 +0530

    first Open source project

---
+++ application/views/startdiv.php
@@ -0,0 +1,4 @@
+<div title="<?php echo($view); ?>" style="position:relative; border:1px dotted red; margin:6px 2px; padding:18px 2px 2px 2px; zoom:1;">
+<div style="position: absolute; left: 0px; top: 0px; padding: 2px 5px; background: red none repeat scroll 0% 0%; color: white; font: 11px Arial; text-align: left ! important; z-index: 998;" onmouseover="this.style.zIndex='999'" onmouseout="this.style.zIndex='998'" title='<?php echo($view); ?>'> 
+<?php echo($view); ?>
+ </div>
